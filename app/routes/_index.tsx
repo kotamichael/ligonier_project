2e78dd7f99e1d4593265386ee3fe0da23e9cd74a
Dakota Michael <dakotamichael@Dakotas-MacBook-Air.local>
2026-05-08 19:12:01 -0400

    Adds loading state

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,17 @@
 import { useTranslation } from "react-i18next"
 import type { MetaFunction } from "react-router"
+import { useNavigation } from "react-router"
 
 export const meta: MetaFunction = () => {
 	return [{ title: "Ligonier Take-Home Challenge" }, { name: "description", content: "Check out these images!" }]
 }
 
 export default function Index() {
+	const navigation = useNavigation()
 	const { t } = useTranslation()
 	return (
-		<div>
-			<h1>{t("Images")}</h1>
+		<div className={navigation.state === "loading" ? "loading" : ""}>
+			<h1>{t("welcome")}</h1>
 		</div>
 	)
 }
