6678cb85af8a160a8705257e4e83b84a1a6cbe1e
Dakota Michael <dakotamichael@Dakotas-MacBook-Air.local>
2026-05-09 17:13:57 -0400

    Adds image key to image

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -26,6 +26,8 @@ export async function loader() {
 	const res = await fetch("https://picsum.photos/v2/list")
 
 	const imageData = await res.json()
+
+	// const sortedImages = imageData.sort((a: Image, b: Image) => Number(a.id) - Number(b.id))
 	return { imagesData: { data: imageData } }
 }
 
@@ -37,10 +39,10 @@ export default function Index({ loaderData }: { loaderData: LoaderData }) {
 		<div className="container mx-auto px-4 py-8">
 			<h1 className="mb-6 font-bold text-3xl">{t("welcome")}</h1>
 			<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-				{loaderData.imagesData.data.map((item) => (
-					<div key={item.id} className="border p-4">
-						<img src={item.download_url} alt={item.author} className="mb-2 h-auto w-full" />
-						<h2 className="font-semibold text-xl">{item.author}</h2>
+				{loaderData.imagesData.data.map((imageRow) => (
+					<div key={imageRow.id} className="border p-4">
+						<img src={imageRow.download_url} key={imageRow.id} alt={imageRow.author} className="mb-2 h-auto w-full" />
+						<h2 className="font-semibold text-xl">{imageRow.author}</h2>
 					</div>
 				))}
 			</div>
